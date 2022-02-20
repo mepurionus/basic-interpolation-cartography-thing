9f706ef87165aba28d290e6bbca36e53022659fa
Mepuri <mepurionus@gmail.com>
2022-02-20 15:14:43 +0100

    Changes

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ## Decided to put it on GitHub because why not
 
-### Version 0.0.1
+### Version 0.0.2
+
+Changed:
++ made script objective
 
 It is my own real app, I have old account on GitHub and it was posted there, wants to have everything centralized.
--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-firstnumber = float(input("Give the value of first point [m - height]: "))
-secondnumber = float(input("Give the value of second point [m - height]: "))
-distance = float(input("Give the value of the distance on the map [mm - distance on map]: "))
+def ask_for_num(what):
+    return float(input(f"Give the value of {what}: "))
+
+class num:
+    def __init__(self, num):
+        self.num = num
+    def give_num(self):
+        return float(self.num)
+
+class printing_stuff:
+    def __init__(self, valueone, valuetwo, round):
+        self.valueone = valueone
+        self.valuetwo = valuetwo
+        self.round = round
+    def print_stuff(self):
+        if self.round == 1:
+            print(f"~{self.valueone} m in terrain = {self.valuetwo} mm on map")
+        else:
+            print(f"{self.valueone} m in terrain = {self.valuetwo} mm on map")
+
+one = num(ask_for_num("first point [m - height]"))
+two = num(ask_for_num("second point [m - height]"))
+dist = num(ask_for_num("the distance on the map [mm - distance on map]"))
+
+firstnumber = one.give_num()
+secondnumber = two.give_num()
+
 if firstnumber < secondnumber:
     firstnumber, secondnumber = secondnumber, firstnumber
+
 difference = firstnumber - secondnumber
-print("%s m in terrain = %s mm on map" % (difference, distance))
+
 supplement = (int((int(firstnumber) - secondnumber)*100))/100
-result = (int((distance * supplement) / difference*1000))/1000
-print("%s m in terrain = %s mm on map" % (supplement, result))
+result = (int((dist.give_num() * (supplement)) / difference*1000))/1000
+
+first = printing_stuff(round(difference), dist.give_num(), 1)
+second = printing_stuff(supplement, result, 0)
+
+first.print_stuff()
+second.print_stuff()
 input("Press ENTER to continue...")
